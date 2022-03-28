900fb65287bdd7272e6b22e60343e361a952d07b
Michael Amir <31745491+mikeamir@users.noreply.github.com>
2022-03-28 13:36:12 +0200

    updated the uploads naming technique

--- src/uploads/uploads.resolver.ts
+++ src/uploads/uploads.resolver.ts
@@ -1,20 +1,34 @@
-import { Args, Mutation, Resolver } from "@nestjs/graphql";
-import { createWriteStream } from "fs";
-import { FileUpload, GraphQLUpload } from "graphql-upload";
+import { Args, Context, Mutation, Resolver } from '@nestjs/graphql';
+import { createWriteStream } from 'fs';
+import { FileUpload, GraphQLUpload } from 'graphql-upload';
 
 @Resolver()
 export class UploadsResolver {
-    @Mutation(() => Boolean)
-    async uploadFile(@Args({ name: 'file', type: () => GraphQLUpload })
-    {
-        createReadStream,
-        filename
-    }: FileUpload): Promise<boolean> {
-        return new Promise(async (resolve, reject) =>
-            createReadStream()
-                .pipe(createWriteStream(`./uploads/${filename}`))
-                .on('finish', () => resolve(true))
-                .on('error', () => reject(false))
-        );
-    }
+  @Mutation(() => String)
+  async uploadFile(
+    @Args({ name: 'file', type: () => GraphQLUpload })
+    { createReadStream, filename }: FileUpload,
+    @Context() ctx,
+  ): Promise<string> {
+    const headers = ctx.req.rawHeaders as Array<string>;
+    const hostUrl = headers[headers.indexOf('Host') + 1];
+    const format = filename.split('.').pop();
+    const date = new Date();
+    const dateString = `${date.getFullYear()}-${date.getMonth() + 1}-${date.getDate()}`;
+    const timeString = `${date.getHours()}-${date.getMinutes()}-${date.getSeconds()}`;
+    const fileName = `${dateString}-${timeString}.${format}`;
+    const newFilePath = `/uploads/${fileName}`;
+    await new Promise(async (resolve, reject) =>
+      createReadStream()
+        .pipe(createWriteStream(`./uploads/${fileName}`))
+        .on('finish', () => {
+          return resolve(newFilePath);
+        })
+        .on('error', (error) => {
+          console.log(error);
+          return reject('error');
+        }),
+    );
+    return `${hostUrl}${newFilePath}`;
+  }
 }
